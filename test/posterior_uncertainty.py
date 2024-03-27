4a4100b35ce09fd410f7395df288f51e81601620
Nishad Gothoskar <nishadg@mit.edu>
2024-03-27 17:45:32 +0000

    posterior

--- test/posterior_uncertainty.py
+++ test/posterior_uncertainty.py
@@ -38,8 +38,10 @@ enumerator = b3d.make_enumerator(["object_pose"])
 camera_pose = Pose.from_position_and_target(jnp.array([0.2, 0.0, 0.0]), jnp.array([0.0, 0.0, 0.0]))
 
 cp_to_pose = lambda cp: Pose(jnp.array([cp[0], cp[1], 0.0]), b3d.Rot.from_rotvec(jnp.array([0.0, 0.0, cp[2]])).as_quat())
-gt_cp = jnp.array([0.0, 0.0, jnp.pi])
 gt_cp = jnp.array([0.0, 0.0, 0])
+gt_cp = jnp.array([0.0, 0.0, 0])
+gt_cp = jnp.array([0.0, 0.0, jnp.pi])
+gt_cp = jnp.array([0.0, 0.0, jnp.pi/2])
 object_pose = cp_to_pose(gt_cp)
 gt_img, _= renderer.render_attribute(
     (camera_pose.inv() @ object_pose).as_matrix()[None,...],
@@ -87,9 +89,9 @@ b3d.rerun_visualize_trace_t(gt_trace, 0)
 
 test_poses = jax.vmap(lambda cp: cp_to_pose(cp))(jnp.stack(
     jnp.meshgrid(
-        jnp.linspace(-0.01, 0.01, 11),
-        jnp.linspace(-0.01, 0.01, 11),
-        jnp.linspace(-jnp.pi/5, jnp.pi/5, 11),
+        jnp.linspace(-0.02, 0.02, 15),
+        jnp.linspace(-0.02, 0.02, 15),
+        jnp.linspace(-jnp.pi/4, jnp.pi/4, 15),
     ),
     axis=-1,
 ).reshape(-1, 3) + gt_cp)
@@ -97,9 +99,6 @@ test_poses = jax.vmap(lambda cp: cp_to_pose(cp))(jnp.stack(
 scores = enumerator.enumerate_choices_get_scores(gt_trace, key, test_poses)
 samples = jax.random.categorical(key, scores, shape=(100,))
 print(samples)
-for i in range(100):
-    trace = enumerator.update_choices(gt_trace, key, test_poses[samples[i]])
-    b3d.rerun_visualize_trace_t(trace, i)
 
 alternate_view_images,_ = renderer.render_attribute_many(
     (alternate_camera_pose.inv() @ test_poses[samples[:100]]).as_matrix()[:,None,...],
@@ -107,50 +106,14 @@ alternate_view_images,_ = renderer.render_attribute_many(
     jnp.array([[0, len(faces)]]),
     vertex_colors
 )
+
 for t in range(100):
+    trace = enumerator.update_choices(gt_trace, key, test_poses[samples[t]])
+    b3d.rerun_visualize_trace_t(trace, t)
     rr.set_time_sequence("frame", t)
     rr.log("alternate_view_image", rr.Image(alternate_view_images[t,...]))
 
 
-var,conc = 0.005, 30.0
 
 
-def accumulate(key, x):
-    keys = jax.random.split(key,2000)
-    test_poses = jax.vmap(b3d.gaussian_vmf_pose.sample,
-        in_axes=(0,None, None, None))(
-        keys,
-        gt_trace["object_pose"],
-        var,conc
-    )
-    scores = enumerator.enumerate_choices_get_scores(gt_trace, keys[-1], test_poses)
-    key = jax.random.split(keys[-1], 1)[0]
-    return key, (test_poses, scores)
-
-_,(poses, scores) =jax.lax.scan(accumulate, key, jnp.arange(100))
-poses = Pose(poses.pos.reshape(-1,3), poses.quat.reshape(-1,4))
-scores = scores.reshape(-1)
-
-
-order = jnp.argsort(scores)[::-1]
-
-for i in range(100):
-    trace = enumerator.update_choices(gt_trace, key, poses[order[i]])
-    b3d.rerun_visualize_trace_t(trace, i)
-
-
-samples = jax.random.categorical(key, jnp.log(scores), shape=(100,))
-print(samples)
-order = jnp.argsort(scores)[::-1]
-
-for i in range(100):
-    trace = enumerator.update_choices(gt_trace, key, test_poses[order[i]])
-    b3d.rerun_visualize_trace_t(trace, i)
-
-
-
-for i in range(100):
-    trace = enumerator.update_choices(gt_trace, key, test_poses[i])
-    b3d.rerun_visualize_trace_t(trace, i)
-
-
+from IPython import embed; embed()

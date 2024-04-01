43fb52e5630f44c89d911e4bcef8dad5d08681f3
Xiaoyan Wang <xxiaoyanwang@gmail.com>
2024-04-01 21:17:25 +0000

    Remove `import b3d` from `setup.py`
    Currently, to install `b3d`, setup.py needs to import `b3d` itself, which can trigger `ModuleNotFoundError` unless users have manually installed the dependencies from `requirements.txt`.
    
    It seems like in `setup.py`, `b3d` pacakge is only used to fill in the version metadata. The proposed change is to sync from the other way around, by setting the version in `setup.py` and read it in `b3d/__init__.py`.

--- b3d/__init__.py
+++ b3d/__init__.py
@@ -5,10 +5,12 @@
 # and any modifications thereto.  Any use, reproduction, disclosure or
 # distribution of this software and related documentation without an express
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
+from pkg_resources import get_distribution
 
-__version__ = "0.0.1"
 from .renderer import *
 from .pose import *
 from .utils import *
 from .model import *
 from .mesh_library import *
+
+__version__ = get_distribution('b3d').version

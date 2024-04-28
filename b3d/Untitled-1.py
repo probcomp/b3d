230170390cbfea4de3ce7da0be33d1088a1492c4
George Matheos <georgematheos@gmail.com>
2024-04-28 12:22:31 +0000

    checkpoint

---
+++ b3d/Untitled-1.py
@@ -0,0 +1,6 @@
+
+TypeError: true_fun and false_fun output must have same type structure, got 
+PyTreeDef((*, (CustomNode(Pose[None], 
+                [CustomNode(Diff[('primal', 'tangent'), (), ()], [*, CustomNode(_NoChange[(), (), ()], [])]), CustomNode(Diff[('primal', 'tangent'), (), ()], [*, CustomNode(_NoChange[(), (), ()], [])])]), [CustomNode(Pose[None], [CustomNode(Diff[('primal', 'tangent'), (), ()], [*, CustomNode(_NoChange[(), (), ()], [])]), CustomNode(Diff[('primal', 'tangent'), (), ()], [*, CustomNode(_NoChange[(), (), ()], [])])])]), *, *))
+and
+PyTreeDef((*, (CustomNode(Pose[None], [*, *]), [CustomNode(Pose[None], [*, *])]), *, *)).

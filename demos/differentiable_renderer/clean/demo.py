d630535a05248b60478cc8f54e998f62805dc9fa
George Matheos <georgematheos@gmail.com>
2024-05-05 18:20:59 +0000

    demo notebook

--- demos/differentiable_renderer/clean/demo.py
+++ demos/differentiable_renderer/clean/demo.py
@@ -45,7 +45,7 @@ particle_colors = jnp.array(
 )
 
 # visualize the scene
-rr.init("differentiable_rendering--scene")
+rr.init("differentiable_rendering--scene_and_renders")
 rr.connect("127.0.0.1:8812")
 rr.log("scene/triangles", rr.Mesh3D(vertex_positions=vertices, indices=faces, vertex_colors=vertex_colors), timeless=True)
 rr.log("scene/camera", rr.Pinhole(focal_length=rendering.fx, width=rendering.image_width, height=rendering.image_height), timeless=True)
@@ -91,6 +91,11 @@ def render_to_dist_from_centers(new_particle_centers):
     )
     return weights, colors
 
+def render_from_centers(new_particle_centers):
+    particle_center_delta  = new_particle_centers - particle_centers
+    new_vertices = vertices.reshape(3, 4, 3) + jnp.expand_dims(particle_center_delta, 1)
+    return rendering.render(new_vertices.reshape(-1, 3), faces.reshape(-1, 3), particle_colors[triangle_to_particle_index], hyperparams)
+
 def compute_logpdf(centers):
     weights, colors = render_to_dist_from_centers(centers)
     return get_img_logpdf(jax.random.PRNGKey(0), color_image, weights, colors)    
@@ -120,3 +125,96 @@ rr.log("logpdfs_of_opengl_rendering_as_green_square_moves/true_object_position",
 #####################
 ### Scene fitting ###
 #####################
+rr.init("differentiable_rendering--scene_fitting3")
+rr.connect("127.0.0.1:8812")
+rr.log("/scene/ground_truth", rr.Mesh3D(vertex_positions=vertices, indices=faces, vertex_colors=vertex_colors), timeless=True)
+rr.log("/scene/camera", rr.Pinhole(focal_length=rendering.fx, width=rendering.image_width, height=rendering.image_height), timeless=True)
+rr.log("/img/opengl_rendering", rr.Image(color_image), timeless=True)
+
+# ULA & MALA inference moves
+# assumes some uniform prior on the particle centers
+@jax.jit
+def ULA_step(key, current_centers, sigma):
+    g = jax.grad(compute_logpdf)(current_centers)
+    stepped = current_centers + sigma**2 * g
+    noised = genjax.normal.sample(key, stepped, jnp.sqrt(2) * sigma)
+    return noised
+
+@jax.jit
+def MALA_step(key, current_centers, sigma):
+    g = jax.grad(compute_logpdf)(current_centers)
+    stepped = current_centers + sigma**2 * g
+    noised = genjax.normal.sample(key, stepped, jnp.sqrt(2) * sigma)
+    
+    # compute acceptance probability
+    logpdf_current = compute_logpdf(current_centers)
+    logpdf_stepped = compute_logpdf(noised)
+    p_ratio = logpdf_stepped - logpdf_current
+
+    q_fwd = genjax.normal.logpdf(noised, stepped, jnp.sqrt(2) * sigma)
+    g_bwd = jax.grad(compute_logpdf)(noised)
+    bwd_stepped = noised + sigma**2 * g_bwd
+    q_bwd = genjax.normal.logpdf(current_centers, bwd_stepped, jnp.sqrt(2) * sigma)
+    q_ratio = q_bwd - q_fwd
+    alpha = jnp.minimum(jnp.exp(p_ratio + q_ratio), 1.0)
+    accept = genjax.bernoulli.sample(key, alpha)
+    return (accept, jnp.where(accept, noised, current_centers))
+grad_jit = jax.jit(jax.grad(compute_logpdf))
+
+# Set up variant of scene misaligned with the real image
+particle_centers_shifted = jnp.array(
+    [
+        [0.05, 0.0, 1.0],
+        [0.15, 0.2, 2.0],
+        [0., 0., 5.]
+    ]
+)
+
+current_centers_gd = particle_centers_shifted
+current_centers_ula = particle_centers_shifted
+current_centers_mala = particle_centers_shifted
+
+sigma = 1e-3
+key = jax.random.PRNGKey(10)
+n_acc_mala = 0
+N_steps = 40
+for i in range(N_steps):
+    print(f"i = {i}")
+    key, subkey = jax.random.split(key)
+    if i != 0:
+        current_centers_gd = current_centers_gd + sigma**2 * grad_jit(current_centers_gd)
+        current_centers_ula = ULA_step(subkey, current_centers_ula, sigma)
+        (acc, current_centers_mala) = MALA_step(subkey, current_centers_mala, sigma)
+        n_acc_mala += acc
+
+    rr.set_time_sequence("scene_fitting_step", i)
+    rendered_gd = render_from_centers(current_centers_gd)
+    rendered_ula = render_from_centers(current_centers_ula)
+    rendered_mala = render_from_centers(current_centers_mala)
+    rr.log("/rerendered/gd", rr.Image(rendered_gd))
+    rr.log("/rerendered/ula", rr.Image(rendered_ula))
+    rr.log("/rerendered/mala", rr.Image(rendered_mala))
+
+    rr.log("/l1_error_imgs/gd", rr.DepthImage(jnp.abs(rendered_gd - color_image).sum(axis=-1)))
+    rr.log("/l1_error_imgs/ula", rr.DepthImage(jnp.abs(rendered_ula - color_image).sum(axis=-1)))
+    rr.log("/l1_error_imgs/mala", rr.DepthImage(jnp.abs(rendered_mala - color_image).sum(axis=-1)))
+
+    for string, centers in [("gd", current_centers_gd), ("ula", current_centers_ula), ("mala", current_centers_mala)]:
+        v, f, c, t2pidx = jax.vmap(
+            utils.center_and_width_to_vertices_faces_colors
+        )(jnp.arange(len(centers)), centers, particle_widths, particle_colors)
+        v, f, c = v.reshape(-1, 3), f.reshape(-1, 3), c.reshape(-1, 3)
+        rr.log(
+            f"scene/{string}",
+            rr.Mesh3D(vertex_positions=v, indices=f)
+        )
+
+    rr.log("/logpdf/gd", rr.Scalar(compute_logpdf(current_centers_gd)))
+    rr.log("/logpdf/ula", rr.Scalar(compute_logpdf(current_centers_ula)))
+    rr.log("/logpdf/mala", rr.Scalar(compute_logpdf(current_centers_mala)))
+
+    rr.log("/l1_errors/gd", rr.Scalar(jnp.abs(rendered_gd - color_image).sum()))
+    rr.log("/l1_errors/ula", rr.Scalar(jnp.abs(rendered_ula - color_image).sum()))
+    rr.log("/l1_errors/mala", rr.Scalar(jnp.abs(rendered_mala - color_image).sum()))
+
+print(f"MALA acceptance fraction: {n_acc_mala / N_steps}")

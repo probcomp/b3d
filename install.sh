df5d73458da86c925cbc5b5bc73db3b2cf9eadfe
George Matheos <georgematheos@gmail.com>
2024-05-14 15:07:34 +0000

    add mkdir needed for tests to pass

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ sudo apt-get install ffmpeg
 pip install --upgrade "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 pip install -e .
 b3d_pull
+mkdir assets/test_results

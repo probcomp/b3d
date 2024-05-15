aac730783d3b01126a51a43dfbb94326706dad1a
George Matheos <georgematheos@gmail.com>
2024-05-15 21:28:45 +0000

    multiple patch tracking demo

--- demos/differentiable_renderer/tracking/multiple_patch_tracking.py
+++ demos/differentiable_renderer/tracking/multiple_patch_tracking.py
@@ -15,6 +15,7 @@ import demos.differentiable_renderer.tracking.utils as utils
 import demos.differentiable_renderer.tracking.model as m
 import b3d.likelihoods as likelihoods
 import b3d.differentiable_renderer as r
+import time
 
 # W = world frame; P = patch frame; C = camera frame
 
@@ -85,13 +86,13 @@ def all_pairs_2(X, Y):
         0, 1
     ).reshape(-1, 2)
 
-width_gradations = jnp.arange(44, 84, 6)
-height_gradations = jnp.arange(38, 96, 6)
+width_gradations = jnp.arange(44, 84, 3)
+height_gradations = jnp.arange(38, 96, 3)
 centers = all_pairs_2(height_gradations, width_gradations)
 
 def get_patches(center):
     center_x, center_y = center[0], center[1]
-    del_pix = 5
+    del_pix = 3
     patch_points_C = jax.lax.dynamic_slice(xyzs_C[0], (center_x-del_pix,center_y-del_pix,0), (2*del_pix,2*del_pix,3)).reshape(-1,3)
     patch_rgbs = jax.lax.dynamic_slice(rgbs[0], (center_x-del_pix,center_y-del_pix,0), (2*del_pix,2*del_pix,3)).reshape(-1,3)
     patch_vertices_C, patch_faces, patch_vertex_colors, patch_face_colors = b3d.make_mesh_from_point_cloud_and_resolution(
@@ -140,3 +141,90 @@ trace, weight = model.importance(
 
 rr.set_time_sequence("frame", 0)
 m.rr_viz_multiobject_trace(trace, renderer)
+
+##### Tracking ######
+#                          b3d.Pose[N]    (H, W, 4) array
+def poses_to_trace_and_weight(poses, observed_rgbd):
+    vcm = genjax.vector_choice_map(genjax.vector_choice_map(genjax.choice(observed_rgbd)))
+    constraints = genjax.choice_map({
+                             "poses": genjax.vector_choice_map(genjax.choice(poses)),
+                             "camera_pose": X_WC,
+                             "observed_rgbd": vcm
+                         })
+    trace, weight = model.importance(jax.random.PRNGKey(0), constraints, (patch_vertices_P, patch_faces, patch_vertex_colors))
+    return trace, weight
+
+@jax.jit
+def poss_quats_to_trace_and_weight(positions, quaternions, observed_rgbd):
+    pose = b3d.Pose(positions, quaternions)
+    return poses_to_trace_and_weight(pose, observed_rgbd)
+
+def poss_quats_to_weight(positions, quaternions, observed_rgbd):
+    return poss_quats_to_trace_and_weight(positions, quaternions, observed_rgbd)[1]
+
+pos_quat_grad_jitted = jax.grad(poss_quats_to_weight, argnums=(0, 1))
+step_pos = 2e-7
+step_quat = 2e-7
+CLIP_STEP = 0.001
+CLIP_QUAT = 0.001
+@jax.jit
+def step_pose(position, quaternion, observed_rgbd):
+    gp, gq = pos_quat_grad_jitted(position, quaternion, observed_rgbd)
+    p_inc = step_pos * gp
+    p_inc = jnp.where((jnp.linalg.norm(p_inc, axis=-1) > CLIP_STEP)[:, None], p_inc / jnp.linalg.norm(p_inc, axis=-1)[:, None] * CLIP_STEP, p_inc)
+    q_inc = step_quat * gq
+    q_inc = jnp.where((jnp.linalg.norm(q_inc, axis=-1) > CLIP_QUAT)[:, None], q_inc / jnp.linalg.norm(q_inc, axis=-1)[:, None] * CLIP_QUAT, q_inc)
+    position = position + p_inc
+    quaternion = quaternion + q_inc
+    quaternion = quaternion / jnp.linalg.norm(quaternion, axis=-1)[:, None]
+    return position, quaternion
+
+# positions = Xs_WP.pos
+# quaternions = Xs_WP._quaternion
+# STEPS_PER_FRAME=20
+# for fr in tqdm(range(observed_rgbds.shape[0])):
+#     for step in tqdm(range(STEPS_PER_FRAME)):
+#         if step > 0:
+#             # position, quaternion = step_pose(position, quaternion, observed_rgbds[fr])
+#             key, subkey = jax.random.split(key)
+#             (positions, quaternions) = step_pose(positions, quaternions, observed_rgbds[fr])
+#         trace, weight = poss_quats_to_trace_and_weight(positions, quaternions, observed_rgbds[fr])
+#         rr.set_time_sequence("gradient_ascent_step1", fr*STEPS_PER_FRAME + step)
+#         m.rr_viz_multiobject_trace(trace, renderer)
+#         rr.log("/trace/logpdf", rr.Scalar(trace.get_score()))
+
+#     rr.set_time_sequence("frames_gradient_tracking", fr)
+#     m.rr_viz_multiobject_trace(trace, renderer)
+
+@jax.jit
+def run_gd_step(carry, fr):
+    positions, quaternions = carry
+    (positions, quaternions) = step_pose(positions, quaternions, observed_rgbds[fr])
+    return ((positions, quaternions), (positions, quaternions))
+
+@jax.jit
+def run_frame(carry, fr):
+    positions, quaternions = carry
+    (positions, quaternions), _ = jax.lax.scan(run_gd_step, (positions, quaternions), jnp.repeat(fr, STEPS_PER_FRAME))
+    return (positions, quaternions), (positions, quaternions)
+
+@jax.jit
+def run_inference(p, q):
+    return jax.lax.scan(run_frame, (p, q), jnp.arange(observed_rgbds.shape[0]))[1]
+
+# carry, seq = jax.lax.scan(run_gd_step, (positions, quaternions), jnp.arange(STEPS_PER_FRAME))
+positions = Xs_WP.pos
+quaternions = Xs_WP._quaternion
+STEPS_PER_FRAME=20
+step_pos = 1e-7
+step_quat = 1e-7
+t = time.time()
+seq = run_inference(positions, quaternions)
+print(time.time() - t)
+for i in range(observed_rgbds.shape[0]):
+    rr.set_time_sequence("frame", i)
+    rr.log("/3D/tracked_points", rr.Points3D(
+        positions = seq[0][i],
+        radii=0.0075*np.ones(seq[0][i].shape[0]),
+        colors=np.repeat(np.array([0,0,255])[None,...], seq[0][i].shape[0], axis=0))
+    )

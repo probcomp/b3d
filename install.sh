2d82486f9f7ed5dcfa186ccb986159614acb6a0b
Nishad Gothoskar <nishadg@mit.edu>
2024-05-20 14:48:43 +0000

    Setup instructions

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 sudo apt-get install mesa-common-dev libegl1-mesa-dev libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev
 sudo apt-get install ffmpeg
-pip install --upgrade "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 pip install -e .
 b3d_pull
-mkdir assets/test_results
+mkdir -p assets/test_results

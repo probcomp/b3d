6b60ff8b5696b8fc283370e9e2ca345219bf57c8
George Matheos <georgematheos@gmail.com>
2024-05-27 16:41:37 +0000

    checkpoint

--- demos/mesh_fitting/demo_depth_init.py
+++ demos/mesh_fitting/demo_depth_init.py
@@ -15,6 +15,7 @@ import demos.mesh_fitting.model as m
 path = os.path.join(
     b3d.get_root_path(),
     "assets/shared_data_bucket/input_data/shout_on_desk.r3d.video_input.npz",
+    # "assets/potted_plant.video_input.npz"
 )
 video_input = b3d.VideoInput.load(path)
 
@@ -46,8 +47,8 @@ def get_vertex_depth(v):
     face_indices = vertex_to_faces[v]
     face_indices_safe = jnp.where(face_indices == -1, 0, face_indices)
     depths = jnp.where(face_indices != -1, triangle_rgbds[face_indices_safe, 3], 0.)
-    n_valid = jnp.sum(face_indices != -1)
-    return depths[0] #jnp.sum(depths) / n_valid
+    n_valid = jnp.sum(depths != 0)
+    return jnp.sum(depths) / n_valid
 vertex_depths = jax.vmap(get_vertex_depth)(jnp.arange(vertices_2D.shape[0]))
 
 vertices_3D = jnp.hstack(
@@ -56,20 +57,20 @@ vertices_3D = jnp.hstack(
 )
 triangle_colors = triangle_rgbds[:, :3]
 
-rr.init("depth_mesh_init")
+rr.init("depth_mesh_init2")
 rr.connect("127.0.0.1:8812")
 v_, f_, vc_ = b3d.utils.triangle_color_mesh_to_vertex_color_mesh(vertices_3D, faces, triangle_colors)
 rr.log(f"/3D/mesh", rr.Mesh3D(
     vertex_positions=v_, indices=f_, vertex_colors=vc_
 ))
 
-initial_mesh = (vertices_3D, faces, triangle_colors)
-
-
 camera_poses = [
     b3d.Pose(video_input.camera_positions[::4][t], video_input.camera_quaternions[::4][t])
     for t in range(rgbs.shape[0])
 ]
+
+initial_mesh = (camera_poses[0].apply(vertices_3D), faces, triangle_colors)
+
 
 mindepth, maxdepth = -1000., 1000.
 color_scale = 0.05
@@ -96,7 +97,7 @@ key = jax.random.PRNGKey(0)
 trace, weight = jax.jit(model.importance)(
     key,
     genjax.choice_map({
-        "vertices": camera_poses[0].apply(initial_mesh[0]),
+        "vertices": initial_mesh[0],
         "faces": initial_mesh[1],
         "face_colors": initial_mesh[2],
         "camera_poses": genjax.vector_choice_map(
@@ -108,3 +109,56 @@ trace, weight = jax.jit(model.importance)(
 )
 
 m.rr_log_trace(trace, renderer, "trace", [0], [0])
+
+frames = [0]
+@jax.jit
+def importance_from_vertices_colors(vertices, colors):
+    trace, weight = jax.jit(model.importance)(
+        key,
+        genjax.choice_map({
+            "vertices": vertices,
+            "faces": initial_mesh[1],
+            "face_colors": colors,
+            "camera_poses": genjax.vector_choice_map(
+                genjax.choice( b3d.Pose.stack_poses([camera_poses[t] for t in frames]))
+            ),
+            "observed_rgbs": genjax.vector_choice_map(genjax.choice_map({"observed_rgb": rgbs[frames, ...]}))
+        }),
+        ()
+    )
+    return trace, weight
+
+def vertices_colors_to_score(vertices, colors):
+    trace, weight = importance_from_vertices_colors(vertices, colors)
+    return weight
+grad_jitted = jax.jit(jax.grad(vertices_colors_to_score, argnums=(0, 1)))
+value_and_grad_jitted = jax.jit(jax.value_and_grad(vertices_colors_to_score, argnums=(0, 1)))
+
+hessian = jax.jit(jax.hessian(vertices_colors_to_score, argnums=(0, 1)))
+
+def vc_to_s(vc):
+    vertices = vc[:1715, :]
+    colors = vc[1715:, :]
+    return vertices_colors_to_score(vertices, colors)
+
+vc = jnp.vstack((initial_mesh[0], initial_mesh[2]))
+
+jax.jacrev(jax.jacrev(vc_to_s))(vc)
+
+
+
+jax.jacrev(vc_to_s)(vc)
+
+jax.hessian(vc_to_s)(vc)
+
+v = jax.jacrev(jax.jacfwd(vc_to_s))(vc)
+
+# h = jax.hessian(vertices_colors_to_score, argnums=(0, 1))(initial_mesh[0], initial_mesh[2])
+
+# g = grad_jitted(initial_mesh[0], initial_mesh[2])
+
+with jax.profiler.trace("/tmp/tensorboard"):
+  # Run the operations to be profiled
+  g = value_and_grad_jitted(initial_mesh[0] + 1.0, initial_mesh[2])
+  g[0].block_until_ready()
+  g[1][0].block_until_ready()

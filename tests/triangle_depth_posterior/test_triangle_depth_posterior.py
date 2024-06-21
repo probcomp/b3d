f31f87b2ad43e994bfeb02f831c6e91936ee2458
George Matheos <georgematheos@gmail.com>
2024-06-21 16:44:20 +0000

    add demo test notebook

--- tests/triangle_depth_posterior/test_triangle_depth_posterior.py
+++ tests/triangle_depth_posterior/test_triangle_depth_posterior.py
@@ -4,16 +4,15 @@ from .task import TrianglePosteriorGridApproximationTask
 from .solver.importance import ImportanceSolver
 import pytest
 
-tasks = [
-    TrianglePosteriorGridApproximationTask.default_scene_using_colors(
-        background_color, triangle_color
-    )
+task_specs = [
+    (background_color, triangle_color)
     for background_color in [jnp.array([1., 1., 1.]), jnp.array([0., 0., 0.])]
     for triangle_color in [jnp.array([1., 0., 0.]), jnp.array([0., 1., 0.])]
 ]
 
-@pytest.mark.parametrize("task", tasks[:3])
-def test(task):
+@pytest.mark.parametrize("task_spec", task_specs)
+def test(task_spec):
+    task = TrianglePosteriorGridApproximationTask.default_scene_using_colors(*task_spec)
     task.run_tests(
         ImportanceSolver(),
         divergence_from_uniform_threshold=jnp.inf

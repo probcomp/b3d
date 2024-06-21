021924594c8cece36a1fca25ed3a1e27e9fe2dca
George Matheos <georgematheos@gmail.com>
2024-06-21 20:41:02 +0000

    remove HGPSFeatureTrackData

--- b3d/io/__init__.py
+++ b3d/io/__init__.py
@@ -1,6 +1,5 @@
 from .feature_track_data import FeatureTrackData
-from .hgps_feature_track_data import HGPSFeatureTrackData
 from .mesh_data import MeshData
 from .video_input import VideoInput
 
-__all__ = [MeshData, FeatureTrackData, VideoInput, HGPSFeatureTrackData]
+__all__ = [MeshData, FeatureTrackData, VideoInput]

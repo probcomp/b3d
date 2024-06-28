480283e46be7e5923d5d0c71a7070bd71632f72c
Mirko <mirko.klukas@gmail.com>
2024-06-28 18:22:37 +0000

    ellipsis test for setter

--- tests/test_particle_system_setters.py
+++ tests/test_particle_system_setters.py
@@ -21,6 +21,7 @@ N = 5
 K = 3
 model, args = get_sparse_test_model_and_args(T, N, K)
 
+
 class MeshTests(unittest.TestCase):
     
     def test_camera_setter(self):
@@ -34,6 +35,18 @@ class MeshTests(unittest.TestCase):
             cams = get_cameras(tr)
             
             assert jnp.allclose(cams[t].pos, jnp.zeros(3))
+            assert jnp.allclose(cams[t].quat, jnp.array([0.,0.,0.,1.]))
+
+
+        key,_ = jax.random.split(key)
+        ch = set_camera_choice(..., Pose(
+            jnp.zeros((T,3)), 
+            jnp.tile(jnp.array([0.,0.,0.,1.]), (T,1))
+        ))
+        tr, w = model.importance(key, ch, args)
+        cams = get_cameras(tr)
+
+        assert jnp.allclose(cams.pos, jnp.zeros((T,3)))
 
 
     def test_observation_setter(self):
@@ -48,3 +61,10 @@ class MeshTests(unittest.TestCase):
 
             # TODO: Wait for vmap importance bug has been resolved
             assert jnp.allclose(uvs[t], jnp.zeros((N,2)))
+
+        key,_ = jax.random.split(key)
+        ch = set_sensor_coordinates_choice(..., jnp.zeros((T,N,2)))
+        tr, w = model.importance(key, ch, args)
+        uvs = get_observations(tr)
+
+        assert jnp.allclose(uvs, jnp.zeros((T,N,2)))

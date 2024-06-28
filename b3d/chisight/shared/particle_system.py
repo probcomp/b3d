5a07ed3f4657d8b720e21670fb5ffbdd1cb9e353
George Matheos <georgematheos@gmail.com>
2024-06-28 19:56:56 +0000

    variable length unfold model

--- b3d/chisight/shared/particle_system.py
+++ b3d/chisight/shared/particle_system.py
@@ -8,7 +8,7 @@ from genjax import gen
 from b3d.chisight.dense.dense_likelihood import make_dense_observation_model, DenseImageLikelihoodArgs
 from b3d import Pose, Mesh
 from b3d.chisight.sparse.gps_utils import add_dummy_var
-from b3d.chisight.sparse.pose_utils import uniform_pose_in_ball
+from b3d.pose.pose_utils import uniform_pose_in_ball
 dummy_mapped_uniform_pose = add_dummy_var(uniform_pose_in_ball).vmap(in_axes=(0,None,None,None))
 
 
@@ -113,7 +113,8 @@ def particle_system_state_step(carried_state, _):
 
 @gen
 def latent_particle_model(
-        num_timesteps, # const object
+        max_num_timesteps, # const object
+        num_timesteps,
         num_particles, # const object
         num_clusters, # const object
         relative_particle_poses_prior_params,
@@ -132,32 +133,49 @@ def latent_particle_model(
         camera_pose_prior_params
     ) @ "state0"
 
-    final_state, scan_retvals = particle_system_state_step.scan(n=(num_timesteps.const - 1))(state0, None) @ "states1+"
+    masked_final_state, masked_scan_retvals = b3d.modeling_utils.masked_scan_combinator(
+        particle_system_state_step,
+        n=(max_num_timesteps.const-1)
+    )(
+        state0,
+        genjax.Mask(
+            # This next line tells the scan combinator how many timesteps to run
+            jnp.arange(max_num_timesteps.const - 1) < num_timesteps - 1,
+            jnp.zeros(max_num_timesteps.const - 1)
+        )
+    ) @ "states1+"
+
 
     # concatenate each element of init_retval, scan_retvals
-    return jax.tree.map(
+    concatenated_states_possibly_invalid = jax.tree.map(
         lambda t1, t2: jnp.concatenate([t1[None, :], t2], axis=0),
-        init_retval, scan_retvals
+        init_retval, masked_scan_retvals.value
     )
+    masked_concatenated_states = genjax.Mask(
+        jnp.concatenate([jnp.array([True]), masked_scan_retvals.flag]),
+        concatenated_states_possibly_invalid
+    )
+    return masked_concatenated_states
 
 @genjax.gen
 def sparse_observation_model(particle_absolute_poses, camera_pose, visibility, instrinsics, sigma):
     # TODO: add visibility
     uv = b3d.camera.screen_from_world(particle_absolute_poses.pos, camera_pose, instrinsics.const)
-    uv_ = genjax.normal(uv, jnp.tile(sigma, uv.shape)) @ "sensor_coordinates"
+    uv_ = b3d.modeling_utils.normal(uv, jnp.tile(sigma, uv.shape)) @ "sensor_coordinates"
     return uv_
 
 @genjax.gen
 def sparse_gps_model(latent_particle_model_args, obs_model_args):
-    # (b3d.camera.Intrinsics.from_array(jnp.array([1.0, 1.0, 1.0, 1.0])), 0.1)
-    particle_dynamics_summary = latent_particle_model(*latent_particle_model_args) @ "particle_dynamics"
-    obs = sparse_observation_model.vmap(in_axes=(0, 0, 0, None, None))(
-        particle_dynamics_summary["absolute_particle_poses"],
-        particle_dynamics_summary["camera_pose"],
-        particle_dynamics_summary["vis_mask"],
+    masked_particle_dynamics_summary = latent_particle_model(*latent_particle_model_args) @ "particle_dynamics"
+    _UNSAFE_particle_dynamics_summary = masked_particle_dynamics_summary.value
+    masked_obs = sparse_observation_model.mask().vmap(in_axes=(0, 0, 0, 0, None, None))(
+        masked_particle_dynamics_summary.flag,
+        _UNSAFE_particle_dynamics_summary["absolute_particle_poses"],
+        _UNSAFE_particle_dynamics_summary["camera_pose"],
+        _UNSAFE_particle_dynamics_summary["vis_mask"],
         *obs_model_args
     ) @ "observation"
-    return (particle_dynamics_summary, obs)
+    return (masked_particle_dynamics_summary, masked_obs)
 
 
 
@@ -166,15 +184,17 @@ def make_dense_gps_model(renderer):
 
     @genjax.gen
     def dense_gps_model(latent_particle_model_args, dense_likelihood_args):
-        # (b3d.camera.Intrinsics.from_array(jnp.array([1.0, 1.0, 1.0, 1.0])), 0.1)
-        particle_dynamics_summary = latent_particle_model(*latent_particle_model_args) @ "particle_dynamics"
-        absolute_particle_poses_last_frame = particle_dynamics_summary["absolute_particle_poses"][-1]
-        camera_pose_last_frame = particle_dynamics_summary["camera_pose"][-1]
+        masked_particle_dynamics_summary = latent_particle_model(*latent_particle_model_args) @ "particle_dynamics"
+        _UNSAFE_particle_dynamics_summary = masked_particle_dynamics_summary.value
+
+        last_timestep_index = jnp.sum(masked_particle_dynamics_summary.flag) - 1
+        absolute_particle_poses_last_frame = _UNSAFE_particle_dynamics_summary["absolute_particle_poses"][last_timestep_index]
+        camera_pose_last_frame = _UNSAFE_particle_dynamics_summary["camera_pose"][last_timestep_index]
         absolute_particle_poses_in_camera_frame = camera_pose_last_frame.inv() @ absolute_particle_poses_last_frame
         
         (meshes, likelihood_args) = dense_likelihood_args
         merged_mesh = Mesh.transform_and_merge_meshes(meshes, absolute_particle_poses_in_camera_frame)
         image = dense_observation_model(merged_mesh, likelihood_args) @ "observation"
-        return (particle_dynamics_summary, image)
+        return (masked_particle_dynamics_summary, image)
 
     return dense_gps_model

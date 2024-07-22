f011f8280b3a83975c99a1df1f299bc832b9f7a2
George Matheos <georgematheos@gmail.com>
2024-07-22 21:30:27 +0000

    update tests to expect passing depth posterior test

--- tests/dense_model_unit_tests/triangle_depth_posterior/test_triangle_depth_posterior.py
+++ tests/dense_model_unit_tests/triangle_depth_posterior/test_triangle_depth_posterior.py
@@ -1,7 +1,10 @@
-import jax
 import jax.numpy as jnp
 from .task import TrianglePosteriorGridApproximationTask
-from .solver.importance import ImportanceSolver
+from .solver.importance import (
+    SimpleLikelihoodImportanceSolver,
+    DiffrendImportanceSolver,
+)
+from b3d.chisight.dense.differentiable_renderer import DifferentiableRendererHyperparams
 import pytest
 
 task_specs = [
@@ -16,4 +19,17 @@ task_specs = [
 @pytest.mark.parametrize("task_spec", task_specs[:1])
 def test(task_spec):
     task = TrianglePosteriorGridApproximationTask.default_scene_using_colors(*task_spec)
-    task.run_tests(ImportanceSolver(), divergence_from_uniform_threshold=jnp.inf)
+
+    # This test should pass with the simple depth likelihood.
+    task.run_tests(SimpleLikelihoodImportanceSolver())
+
+    # I also expect this to pass with the differentiable likelihood
+    # when the sigma parameter is extremely low.
+    task.run_tests(
+        DiffrendImportanceSolver(DifferentiableRendererHyperparams(3, 1e-15, 1e-2, -1))
+    )
+
+    # As of July 22, 2024, I don't expect the test to pass
+    # using the differentiable renderer with
+    # the hyperparameters I tested this renderer with for
+    # gradient-based patch tracking.

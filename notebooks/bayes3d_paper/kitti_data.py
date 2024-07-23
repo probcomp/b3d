13b4fc5761f34d09e0ad4d58d8256819d6ffd3de
Nishad Gothoskar <nishadg@mit.edu>
2024-07-23 20:50:05 +0000

    save

--- notebooks/bayes3d_paper/kitti_data.py
+++ notebooks/bayes3d_paper/kitti_data.py
@@ -13,7 +13,7 @@ b3d.rr_init("kitti")
 basedir = os.path.join(b3d.get_assets_path(), "kitti")
 date = "2011_09_26"
 drive = "0005"
-frames = range(0, 153, 5)
+frames = range(0, 153, 2)
 dataset = pykitti.raw(basedir, date, drive, frames=frames)
 
 
@@ -72,41 +72,7 @@ b3d.rr.log("depth/rgb", b3d.rr.Image(rgbs[0]), timeless=True)
 
 CORRECT_MASK_INDEX = 0
 
-mask = jnp.array(masks[0]["segmentation"])
-
-xyzs = b3d.xyz_from_depth(depths[0], fx, fy, cx, cy)[mask]
-colors = rgbs[0][mask]
-colors = colors[xyzs.sum(-1) > 0]
-xyzs = xyzs[xyzs.sum(-1) > 0]
-m = b3d.segment_point_cloud(xyzs, threshold=0.2) == 0
-
-xyzs = xyzs[m]
-colors = colors[m]
-
-resolution = 0.05
-meshes = b3d.mesh.transform_mesh(
-    jax.vmap(b3d.mesh.Mesh.cube_mesh)(
-        jnp.ones((xyzs.shape[0], 3)) * resolution * 2.0, colors / 255.0
-    ),
-    b3d.Pose.from_translation(xyzs)[:, None],
-)
-full_mesh = b3d.mesh.Mesh.squeeze_mesh(meshes)
-full_mesh.rr_visualize("mesh")
-
-pose = b3d.Pose.from_translation(full_mesh.vertices.mean(0))
-full_mesh.vertices = pose.inv().apply(full_mesh.vertices)
-full_mesh.rr_visualize("mesh")
-
-renderer = b3d.RendererOriginal(width, height, fx, fy, cx, cy, 0.01, 100.0)
-rgbd = renderer.render_rgbd_from_mesh(full_mesh.transform(pose))
-b3d.rr.log("rerender", b3d.rr.Image(rgbd[..., :3]))
-
-b3d.rr.log("rerender/depth", b3d.rr.DepthImage(rgbd[..., 3]))
-
-rgbs = jnp.array(rgbs)
-depths = jnp.array(depths)
-mask.shape
-
+masks_jnp = jnp.array([mask["segmentation"] for mask in masks])
 
 kitti_data_path = (
     b3d.get_assets_path()
@@ -117,18 +83,67 @@ np.savez(
     rgb=np.array(rgbs) / 255.0,
     depths=np.array(depths),
     camera_intrinsics=np.array([fx, fy, cx, cy, 0.001, 100.0]),
-    object_position=np.array(pose.pos[None, ...]),
-    object_quaternion=np.array(pose.quat[None, ...]),
-    mask=np.array(mask[None, ...]),
+    masks=np.array(masks_jnp),
 )
 
-kitti_mesh_path = (
-    b3d.get_assets_path()
-    / "shared_data_bucket/foundation_pose_tracking_datasets/kitti_initial_data.obj"
-)
-full_mesh.save(kitti_mesh_path)
+# xyzs = b3d.xyz_from_depth(depths[0], fx, fy, cx, cy)[mask]
+# colors = rgbs[0][mask]
+# colors = colors[xyzs.sum(-1) > 0]
+# xyzs = xyzs[xyzs.sum(-1) > 0]
+# m = b3d.segment_point_cloud(xyzs, threshold=0.2) == 0
 
-d = np.load(kitti_data_path)
+# xyzs = xyzs[m]
+# colors = colors[m]
 
-for i, v in d.items():
-    print(i, v.shape)
+# resolution = 0.05
+# meshes = b3d.mesh.transform_mesh(
+#     jax.vmap(b3d.mesh.Mesh.cube_mesh)(
+#         jnp.ones((xyzs.shape[0], 3)) * resolution * 2.0, colors / 255.0
+#     ),
+#     b3d.Pose.from_translation(xyzs)[:, None],
+# )
+# full_mesh = b3d.mesh.Mesh.squeeze_mesh(meshes)
+# full_mesh.rr_visualize("mesh")
+
+# pose = b3d.Pose.from_translation(full_mesh.vertices.mean(0))
+# full_mesh.vertices = pose.inv().apply(full_mesh.vertices)
+# full_mesh.rr_visualize("mesh")
+
+# renderer = b3d.RendererOriginal(width, height, fx, fy, cx, cy, 0.01, 100.0)
+# rgbd = renderer.render_rgbd_from_mesh(full_mesh.transform(pose))
+# b3d.rr.log("rerender", b3d.rr.Image(rgbd[..., :3]))
+
+# b3d.rr.log("rerender/depth", b3d.rr.DepthImage(rgbd[..., 3]))
+
+# rgbs = jnp.array(rgbs)
+# depths = jnp.array(depths)
+# mask.shape
+
+
+# kitti_data_path = (
+#     b3d.get_assets_path()
+#     / "shared_data_bucket/foundation_pose_tracking_datasets/kitti_initial_data.npz"
+# )
+# np.savez(
+#     kitti_data_path,
+#     rgb=np.array(rgbs) / 255.0,
+#     depths=np.array(depths),
+#     camera_intrinsics=np.array([fx, fy, cx, cy, 0.001, 100.0]),
+#     object_position=np.array(pose.pos[None, ...]),
+#     object_quaternion=np.array(pose.quat[None, ...]),
+#     mask=np.array(mask[None, ...]),
+# )
+
+# kitti_mesh_path = (
+#     b3d.get_assets_path()
+#     / "shared_data_bucket/foundation_pose_tracking_datasets/kitti_initial_data.obj"
+# )
+# full_mesh.save(kitti_mesh_path)
+
+# import numpy as np
+# import b3d
+
+# d = np.load(kitti_data_path)
+
+# for i, v in d.items():
+#     print(i, v.shape)

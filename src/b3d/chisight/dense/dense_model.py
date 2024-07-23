618a9d4a02f8ff9fd216fe11f9bcf5ba4244e275
Nishad Gothoskar <nishadg@mit.edu>
2024-07-23 14:21:13 +0000

    save

--- src/b3d/chisight/dense/dense_model.py
+++ src/b3d/chisight/dense/dense_model.py
@@ -7,24 +7,20 @@ from b3d import Mesh, Pose
 from b3d.modeling_utils import uniform_pose
 
 
-def make_dense_multiobject_model(renderer, image_likelihood_func):
-    image_likelihood = (
-        b3d.chisight.dense.likelihoods.image_likelihood.make_image_likelihood(
-            image_likelihood_func
-        )
-    )
-
+def make_dense_multiobject_model(renderer, image_likelihood):
     @genjax.gen
     def dense_multiobject_model(args_dict):
         meshes = args_dict["meshes"]
         likelihood_args = args_dict["likelihood_args"]
         num_objects = args_dict["num_objects"]
 
-        outlier_probability = genjax.uniform(0.001, 1.0) @ "outlier_probability"
-        color_variance = genjax.uniform(0.0, 1.0) @ "color_variance"
-        depth_variance = genjax.uniform(0.0, 1.0) @ "depth_variance"
+        outlier_probability = genjax.uniform(0.0001, 0.9999) @ "outlier_probability"
+        lightness_variance = genjax.uniform(0.0001, 1.0) @ "lightness_variance"
+        color_variance = genjax.uniform(0.0001, 1.0) @ "color_variance"
+        depth_variance = genjax.uniform(0.0001, 1.0) @ "depth_variance"
 
         likelihood_args["outlier_probability"] = outlier_probability
+        likelihood_args["lightness_variance"] = lightness_variance
         likelihood_args["color_variance"] = color_variance
         likelihood_args["depth_variance"] = depth_variance
 
@@ -37,15 +33,21 @@ def make_dense_multiobject_model(renderer, image_likelihood_func):
             all_poses.append(object_pose)
         all_poses = Pose.stack_poses(all_poses)
 
-        scene_mesh = Mesh.transform_and_merge_meshes(meshes, all_poses)
+        camera_pose = (
+            uniform_pose(jnp.ones(3) * -100.0, jnp.ones(3) * 100.0) @ "camera_pose"
+        )
+
+        scene_mesh = Mesh.transform_and_merge_meshes(meshes, all_poses).transform(
+            camera_pose.inv()
+        )
         latent_rgbd = renderer.render_rgbd_from_mesh(scene_mesh)
 
-        image = image_likelihood(latent_rgbd, likelihood_args) @ "image"
+        image = image_likelihood(latent_rgbd, likelihood_args) @ "rgbd"
         return {
             "likelihood_args": likelihood_args,
             "scene_mesh": scene_mesh,
             "latent_rgbd": latent_rgbd,
-            "image": image,
+            "rgbd": image,
         }
 
     return dense_multiobject_model

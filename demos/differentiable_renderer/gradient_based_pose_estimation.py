c35c30b073d0fb54da295d0087db29fb28800e33
Nishad Gothoskar <nishadg@mit.edu>
2024-07-23 01:51:26 +0000

    optimization

--- demos/differentiable_renderer/gradient_based_pose_estimation.py
+++ demos/differentiable_renderer/gradient_based_pose_estimation.py
@@ -6,7 +6,7 @@ import os
 import trimesh
 import b3d
 from jax.scipy.spatial.transform import Rotation as Rot
-from b3d import Pose
+from b3d import Pose, Mesh
 import rerun as rr
 import functools
 import genjax
@@ -15,8 +15,6 @@ import jax
 import jax.numpy as jnp
 import optax
 import b3d.chisight.dense.differentiable_renderer as rendering
-import b3d.likelihoods as likelihoods
-from b3d.renderer_original import RendererOriginal
 import demos.differentiable_renderer.utils as utils
 from functools import partial
 
@@ -34,28 +32,6 @@ def map_nested_fn(fn):
         }
 
     return map_fn
-
-
-# Set up OpenGL renderer
-image_width = 200
-image_height = 200
-fx = 150.0
-fy = 150.0
-cx = 100.0
-cy = 100.0
-near = 0.001
-far = 16.0
-renderer = RendererOriginal(image_width, image_height, fx, fy, cx, cy, near, far)
-
-WINDOW = 5
-
-mesh_path = os.path.join(
-    b3d.get_root_path(),
-    "assets/shared_data_bucket/ycb_video_models/models/006_mustard_bottle/textured_simple.obj",
-)
-mesh = trimesh.load(mesh_path)
-object_library = b3d.MeshLibrary.make_empty_library()
-object_library.add_trimesh(mesh)
 
 
 def render_to_dist_params(
@@ -83,7 +59,7 @@ def render_to_dist_params(
     The remaining weights are those assigned to some triangles in the scene.
     The attributes measured on those triangles are contained in `attributes`.
     """
-    image = renderer.rasterize(vertices[None, ...], faces)
+    image = renderer.rasterize_many(vertices[None, ...], faces)
     triangle_id_image = image[0, ..., -1].astype(jnp.int32)
 
     triangle_intersected_padded = jnp.pad(
@@ -159,35 +135,58 @@ def render_to_average_rgbd(
 hyperparams = rendering.DifferentiableRendererHyperparams(3, 5e-5, 0.25, -1)
 
 
-def render(params):
+def render(params, mesh_params):
     image = render_to_average_rgbd(
         renderer,
         b3d.Pose(params["position"], params["quaternion"]).apply(
-            object_library.vertices
+            mesh_params["vertices"]
         ),
-        object_library.faces,
-        object_library.attributes,
+        mesh_params["faces"],
+        mesh_params["vertex_attributes"],
         background_attribute=jnp.array([0.0, 0.0, 0.0, 0]),
         hyperparams=hyperparams,
     )
     return image
 
 
+# Set up OpenGL renderer
+image_width = 200
+image_height = 200
+fx = 150.0
+fy = 150.0
+cx = 100.0
+cy = 100.0
+near = 0.001
+far = 16.0
+renderer = b3d.RendererOriginal(image_width, image_height, fx, fy, cx, cy, near, far)
+
+WINDOW = 5
+
+mesh_path = os.path.join(
+    b3d.get_root_path(),
+    "assets/shared_data_bucket/ycb_video_models/models/006_mustard_bottle/textured_simple.obj",
+)
+mesh = Mesh.from_obj(mesh_path)
+
+
 render_jit = jax.jit(render)
 
-
-vertices, faces = object_library.vertices, object_library.faces
-image = renderer.rasterize(vertices[None, ...], faces)
-
+mesh_params = {
+    "vertices": mesh.vertices,
+    "faces": mesh.faces,
+    "vertex_attributes": mesh.vertex_attributes,
+}
 gt_pose = Pose.from_position_and_target(
     jnp.array([0.3, 0.3, 0.0]),
     jnp.array([0.0, 0.0, 0.0]),
 ).inv()
-gt_image = render_jit({"position": gt_pose.position, "quaternion": gt_pose.quaternion})
+gt_image = render_jit(
+    {"position": gt_pose.position, "quaternion": gt_pose.quaternion}, mesh_params
+)
 
 
-def loss_func_rgbd(params, gt):
-    image = render(params)
+def loss_func_rgbd(params, mesh_params, gt):
+    image = render(params, mesh_params)
     return jnp.mean(jnp.abs(image[..., :3] - gt[..., :3]))
     #  + jnp.mean(jnp.abs(image[...,3] - gt[...,3]))
 
@@ -198,7 +197,7 @@ loss_func_rgbd_grad = jax.value_and_grad(loss_func_rgbd, argnums=(0,))
 @partial(jax.jit, static_argnums=(1,))
 def step(carry, tx):
     (params, gt_image, state) = carry
-    loss, (gradients,) = loss_func_rgbd_grad(params, gt_image)
+    loss, (gradients,) = loss_func_rgbd_grad(params, mesh_params, gt_image)
     updates, state = tx.update(gradients, state, params)
     params = optax.apply_updates(params, updates)
     return ((params, gt_image, state), None)
@@ -225,21 +224,19 @@ params = {
 }
 
 rr.log("image", rr.Image(gt_image[..., :3]), timeless=True)
-rr.log("cloud", rr.Points3D(gt_pose.apply(object_library.vertices)), timeless=True)
+rr.log("cloud", rr.Points3D(gt_pose.apply(mesh.vertices)), timeless=True)
 
 pbar = tqdm(range(200))
 state = tx.init(params)
-images = [render_jit(params)]
+images = [render_jit(params, mesh_params)]
 for t in pbar:
     (params, gt_image, state), _ = step((params, gt_image, state), tx)
     rr.set_time_sequence("frame", t)
-    image = render_jit(params)
+    image = render_jit(params, mesh_params)
     rr.log("image/reconstruction", rr.Image(image[..., :3]))
     rr.log(
         "cloud/reconstruction",
         rr.Points3D(
-            b3d.Pose(params["position"], params["quaternion"]).apply(
-                object_library.vertices
-            )
+            b3d.Pose(params["position"], params["quaternion"]).apply(mesh.vertices)
         ),
     )

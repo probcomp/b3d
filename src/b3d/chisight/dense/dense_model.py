04f75e509165d490752ec23dc86e2aef1bca6f43
Nishad Gothoskar <nishadg@mit.edu>
2024-07-24 18:31:49 +0000

    save before color exploration

--- src/b3d/chisight/dense/dense_model.py
+++ src/b3d/chisight/dense/dense_model.py
@@ -36,17 +36,19 @@ def make_dense_multiobject_model(renderer, likelihood_func, sample_func=None):
         likelihood_args = args_dict["likelihood_args"]
         num_objects = args_dict["num_objects"]
 
-        outlier_probability = genjax.uniform(0.0001, 0.9999) @ "outlier_probability"
-        lightness_variance = genjax.uniform(0.0001, 1.0) @ "lightness_variance"
-        color_variance = genjax.uniform(0.0001, 1.0) @ "color_variance"
-        depth_variance = genjax.uniform(0.0001, 1.0) @ "depth_variance"
-        blur = genjax.uniform(0.0001, 1.0) @ "blur"
+        outlier_probability = (
+            genjax.uniform(0.0001, 1.0) @ f"outlier_probability_background"
+        )
+        lightness_variance = (
+            genjax.uniform(0.0001, 1.0) @ f"lightness_variance_background"
+        )
+        color_variance = genjax.uniform(0.0001, 1.0) @ f"color_variance_background"
+        depth_variance = genjax.uniform(0.0001, 1.0) @ f"depth_variance_background"
 
-        likelihood_args["outlier_probability"] = outlier_probability
-        likelihood_args["lightness_variance"] = lightness_variance
-        likelihood_args["color_variance"] = color_variance
-        likelihood_args["depth_variance"] = depth_variance
-        likelihood_args["blur"] = blur
+        likelihood_args[f"outlier_probability_background"] = outlier_probability
+        likelihood_args[f"lightness_variance_background"] = lightness_variance
+        likelihood_args[f"color_variance_background"] = color_variance
+        likelihood_args[f"depth_variance_background"] = depth_variance
 
         all_poses = []
         for i in range(num_objects.const):
@@ -54,6 +56,19 @@ def make_dense_multiobject_model(renderer, likelihood_func, sample_func=None):
                 uniform_pose(jnp.ones(3) * -100.0, jnp.ones(3) * 100.0)
                 @ f"object_pose_{i}"
             )
+
+            outlier_probability = (
+                genjax.uniform(0.0001, 1.0) @ f"outlier_probability_{i}"
+            )
+            lightness_variance = genjax.uniform(0.0001, 1.0) @ f"lightness_variance_{i}"
+            color_variance = genjax.uniform(0.0001, 1.0) @ f"color_variance_{i}"
+            depth_variance = genjax.uniform(0.0001, 1.0) @ f"depth_variance_{i}"
+
+            likelihood_args[f"outlier_probability_{i}"] = outlier_probability
+            likelihood_args[f"lightness_variance_{i}"] = lightness_variance
+            likelihood_args[f"color_variance_{i}"] = color_variance
+            likelihood_args[f"depth_variance_{i}"] = depth_variance
+
             all_poses.append(object_pose)
         all_poses = Pose.stack_poses(all_poses)
 

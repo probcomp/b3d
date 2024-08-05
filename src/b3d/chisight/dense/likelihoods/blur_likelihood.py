aad0bb0d6a5e518d29fc60a1f6acb1abb73e28a5
Nishad Gothoskar <nishadg@mit.edu>
2024-08-05 21:11:07 +0000

    modify likelihood interface

--- src/b3d/chisight/dense/likelihoods/blur_likelihood.py
+++ src/b3d/chisight/dense/likelihoods/blur_likelihood.py
@@ -1,8 +1,9 @@
 import functools
 
-import genjax
 import jax
 import jax.numpy as jnp
+
+import b3d
 
 
 def log_gaussian_kernel(size: int, sigma: float) -> jnp.ndarray:
@@ -14,216 +15,94 @@ def log_gaussian_kernel(size: int, sigma: float) -> jnp.ndarray:
     return kernel
 
 
-lower_bound = jnp.array([0.0, 0.0, 0.0, 0.0])
-upper_bound = jnp.array([1.0, 1.0, 1.0, 3.0])
-
-filter_size = 4
+filter_half_width = 4
 
 
 @jax.jit
-def blur_intermediate_sample_func(key, latent_rgbd, likelihood_args):
-    color_variance = likelihood_args["color_variance_0"]
-    depth_variance = likelihood_args["depth_variance_0"]
-    outlier_probability = likelihood_args["outlier_probability_0"]
+def blur_intermediate_likelihood_func(observed_rgbd, likelihood_args):
+    fx = likelihood_args["fx"]
+    fy = likelihood_args["fy"]
+    cx = likelihood_args["cx"]
+    cy = likelihood_args["cy"]
 
-    ###########
-    @functools.partial(
-        jnp.vectorize,
-        signature="(m),(2)->(4)",
-        excluded=(
-            2,
-            3,
-            4,
-        ),
+    rasterize_results = likelihood_args["rasterize_results"]
+    triangle_collision_indices_image = rasterize_results[..., 3].astype(jnp.int32) - 1
+    valid = triangle_collision_indices_image >= 0
+
+    latent_rgbd = likelihood_args["latent_rgbd"]
+    mesh_transformed = likelihood_args["scene_mesh"]
+
+    far_plane_xyz = b3d.xyz_from_depth(
+        jnp.ones((latent_rgbd.shape[0], latent_rgbd.shape[1])), fx, fy, cx, cy
     )
-    def per_pixel(
-        ij,
-        key,
-        latent_rgbd_padded,
-        log_kernel,
-        filter_size,
-    ):
-        latent_rgb_padded_window = jax.lax.dynamic_slice(
-            latent_rgbd_padded,
-            (ij[0], ij[1], 0),
-            (2 * filter_size + 1, 2 * filter_size + 1, 4),
-        )
-        # outliers_padded = jax.random.uniform(
-        #     key, (2 * filter_size + 1, 2 * filter_size + 1, 4), minval=0.0, maxval=1.0
-        # )
+    vertices_mean_image = (
+        valid[..., None]
+        * mesh_transformed.vertices[
+            mesh_transformed.faces[triangle_collision_indices_image]
+        ].mean(axis=-2)
+        + ~valid[..., None] * far_plane_xyz
+    )
+    colors_image = latent_rgbd[..., :3]
 
-        # no_mesh = latent_rgb_padded_window[..., 3] == 0.0
-        # latent_rgb_padded_window = (
-        #     (1 - no_mesh)[...,None] * latent_rgb_padded_window
-        #     + no_mesh[...,None] * outliers_padded
-        # )
-
-        index = jax.random.categorical(key, log_kernel.flatten())
-        sampled_rgbd_value = latent_rgb_padded_window.reshape(-1, 4)[index]
-        noisy_rgbd_value = genjax.truncated_normal.sample(
-            key,
-            sampled_rgbd_value,
-            jnp.array([color_variance, color_variance, color_variance, depth_variance]),
-            0.0,
-            1.0,
-        )
-
-        is_outlier = genjax.bernoulli.sample(
-            key, jax.scipy.special.logit(outlier_probability)
-        )
-        outlier_value = jax.random.uniform(
-            key, (4,), minval=jnp.zeros(4), maxval=jnp.ones(4)
-        )
-
-        return is_outlier * outlier_value + (1 - is_outlier) * noisy_rgbd_value
-
-    @jax.jit
-    def likelihood_per_pixel(latent_rgbd: jnp.ndarray, blur):
-        latent_rgbd = (latent_rgbd - lower_bound) / (upper_bound - lower_bound)
-
-        latent_rgbd_padded = jnp.pad(
-            latent_rgbd,
-            (
-                (filter_size, filter_size),
-                (filter_size, filter_size),
-                (0, 0),
-            ),
-            mode="edge",
-        )
-        jj, ii = jnp.meshgrid(
-            jnp.arange(latent_rgbd.shape[1]), jnp.arange(latent_rgbd.shape[0])
-        )
-        indices = jnp.stack([ii, jj], axis=-1)
-
-        log_kernel = log_gaussian_kernel(2 * filter_size + 1, blur)
-
-        keys = jax.random.split(key, (latent_rgbd.shape[0], latent_rgbd.shape[1]))
-        values = per_pixel(
-            indices,
-            keys,
-            latent_rgbd_padded,
-            log_kernel,
-            filter_size,
-        )
-        return values * (upper_bound - lower_bound) + lower_bound
-
-    # noisy_image = genjax.truncated_normal.sample(key, latent_rgbd, color_variance, lower_bound, upper_bound)
-    # return noisy_image
-    return likelihood_per_pixel(latent_rgbd, likelihood_args["blur"])
-
-
-@jax.jit
-def blur_intermediate_likelihood_func(observed_rgbd, latent_rgbd, likelihood_args):
-    # k = likelihood_args["k"].const
     color_variance = likelihood_args["color_variance_0"]
     depth_variance = likelihood_args["depth_variance_0"]
     outlier_probability = likelihood_args["outlier_probability_0"]
+    blur = likelihood_args["blur"]
+
     rows = likelihood_args["rows"]
     cols = likelihood_args["cols"]
 
     ###########
     @functools.partial(
         jnp.vectorize,
-        signature="(m)->(),()",
-        excluded=(
-            1,
-            2,
-            3,
-            4,
-        ),
+        signature="(m)->()",
     )
-    def per_pixel(
-        ij,
-        observed_rgbd,
-        latent_rgbd_padded,
-        blur,
-        filter_size,
-    ):
-        log_kernel = log_gaussian_kernel(2 * filter_size + 1, blur)
+    def score_pixel(ij):
+        lower_indices = (ij[0] - filter_half_width, ij[1] - filter_half_width, 0)
+        length = (2 * filter_half_width + 1, 2 * filter_half_width + 1, 3)
+        vertices_mean_window = jax.lax.dynamic_slice(
+            vertices_mean_image, lower_indices, length
+        )
+        colors_window = jax.lax.dynamic_slice(colors_image, lower_indices, length)
+        valid_window = jax.lax.dynamic_slice(valid, lower_indices[:2], length[:2])
 
-        latent_rgb_padded_window = jax.lax.dynamic_slice(
-            latent_rgbd_padded,
-            (ij[0], ij[1], 0),
-            (2 * filter_size + 1, 2 * filter_size + 1, 4),
+        observed_color = observed_rgbd[ij[0], ij[1], :3]
+        observed_depth = observed_rgbd[ij[0], ij[1], 3]
+        depth_probability = jax.scipy.stats.norm.logpdf(
+            observed_depth,
+            vertices_mean_window[..., 2],
+            depth_variance,
+        )
+        color_probability = jax.scipy.stats.norm.logpdf(
+            observed_color,
+            colors_window,
+            color_variance,
+        )
+        probability = color_probability.sum(-1) + depth_probability
+        scores_inlier = valid_window * probability
+
+        pixel_coordinates = b3d.xyz_to_pixel_coordinates(
+            vertices_mean_window, fx, fy, cx, cy
         )
 
-        error = jnp.abs(latent_rgb_padded_window - observed_rgbd[ij[0], ij[1], :])
-        inlier = jnp.all(
-            error
-            < jnp.array(
-                [color_variance, color_variance, color_variance, depth_variance]
-            ),
-            axis=-1,
-        )
-
-        valid = latent_rgb_padded_window[..., 3] != 0.0
-
-        scores_inlier = (valid * inlier) * 5.00 + (valid * ~inlier) * -jnp.inf
-
-        # scores_inlier = genjax.truncated_normal.logpdf(
-        #     observed_rgbd[ij[0], ij[1], :],
-        #     latent_rgb_padded_window,
-        #     jnp.array([color_variance, color_variance, color_variance, depth_variance]),
-        #     0.0 - 0.00001,
-        #     1.0 + 0.00001,
-        # ).sum(-1)
-
-        # no_mesh = latent_rgb_padded_window[..., 3] == 0.
-        # outlier_probability_adjusted = (no_mesh) * 1.0 + (1 - no_mesh) * outlier_probability
+        _log_kernel = ((pixel_coordinates - ij) ** 2).sum(-1) * -1.0 / (2.0 * blur**2)
+        log_kernel = _log_kernel - jax.nn.logsumexp(_log_kernel)
+        log_kernel = log_kernel - jax.nn.logsumexp(log_kernel)
 
         score_mixed = jax.nn.logsumexp(scores_inlier + log_kernel)
-
         final_score = jnp.logaddexp(
             score_mixed + jnp.log(1.0 - outlier_probability),
             jnp.log(outlier_probability),
         )
-        return score_mixed, final_score
+        return final_score
 
-    @jax.jit
-    def likelihood_per_pixel(
-        observed_rgbd: jnp.ndarray, latent_rgbd: jnp.ndarray, blur
-    ):
-        # observed_rgbd = (observed_rgbd - lower_bound) / (upper_bound - lower_bound)
-        # latent_rgbd = (latent_rgbd - lower_bound) / (upper_bound - lower_bound)
-
-        latent_rgbd_padded = jnp.pad(
-            latent_rgbd,
-            (
-                (filter_size, filter_size),
-                (filter_size, filter_size),
-                (0, 0),
-            ),
-            mode="edge",
-        )
-        # jj, ii = jnp.meshgrid(
-        #     jnp.arange(observed_rgbd.shape[1]), jnp.arange(observed_rgbd.shape[0])
-        # )
-        indices = jnp.stack([rows, cols], axis=-1)
-
-        scores_inlier, scores_final = per_pixel(
-            indices,
-            observed_rgbd,
-            latent_rgbd_padded,
-            blur,
-            filter_size,
-        )
-        return scores_inlier, scores_final
-
-    _, scores = likelihood_per_pixel(
-        observed_rgbd, latent_rgbd, likelihood_args["blur"]
-    )
-
-    # score = genjax.truncated_normal.logpdf(observed_rgbd, latent_rgbd, color_variance, lower_bound, upper_bound)[...,:3].sum()
-    # score = (jax.nn.logsumexp(pixelwise_score) - jnp.log(pixelwise_score.size)) * k
-    score = scores.sum()
+    indices = jnp.stack([rows, cols], axis=-1)
+    scores = score_pixel(indices)
 
     pixelwise_score = jnp.zeros((observed_rgbd.shape[0], observed_rgbd.shape[1]))
     pixelwise_score = pixelwise_score.at[rows, cols].set(scores)
 
     return {
-        "score": score,
-        "scores": scores,
-        "observed_color_space_d": observed_rgbd,
-        "latent_color_space_d": latent_rgbd,
+        "score": scores.sum(),
         "pixelwise_score": pixelwise_score,
     }

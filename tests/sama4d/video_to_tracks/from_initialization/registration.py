32863652b1b62a7894d6f25e6dc820dec2d5ac99
Mirko <mirko.klukas@gmail.com>
2024-08-22 15:01:10 +0000

    ruff checked

--- tests/sama4d/video_to_tracks/from_initialization/registration.py
+++ tests/sama4d/video_to_tracks/from_initialization/registration.py
@@ -2,8 +2,9 @@
 This file registers a default set of tasks and solvers for the video to keypoint tracks task class.
 """
 
-import b3d
 import jax.numpy as jnp
+
+import b3d
 
 from ...data_curation import get_loaders_for_all_curated_scenes
 from .keypoint_tracking_task import KeypointTrackingTask

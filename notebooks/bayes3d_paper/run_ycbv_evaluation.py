bbac612f1605f318b59b7fa77e551ac31280d008
Nishad Gothoskar <nishadg@mit.edu>
2024-08-27 04:59:05 +0000

    tracking range expanded

--- notebooks/bayes3d_paper/run_ycbv_evaluation.py
+++ notebooks/bayes3d_paper/run_ycbv_evaluation.py
@@ -61,6 +61,75 @@ def run_tracking(scene=None, object=None, debug=False):
             4.0,
         )
 
+        def grid_outlier_prob(trace, values):
+            return jax.vmap(
+                lambda x: info_from_trace(
+                    b3d.update_choices(trace, Pytree.const(("outlier_probability",)), x)
+                )["scores"]
+            )(values)
+
+        @jax.jit
+        def update_pose_and_color(trace, address, pose):
+            trace = b3d.update_choices(trace, address, pose)
+            info = info_from_trace(trace)
+            current_outlier_probabilities = trace.get_choices()["outlier_probability"]
+            model_rgbd, observed_rgbd = (
+                info["model_rgbd"],
+                info["corresponding_observed_rgbd"],
+            )
+            deltas = (observed_rgbd - model_rgbd)[..., :3]
+            deltas_clipped = jnp.clip(deltas, -0.1, 0.1)
+
+            mesh = trace.get_args()[0]["meshes"][0]
+            is_inlier = current_outlier_probabilities == outlier_probability_sweep[0]
+            mesh.vertex_attributes = (
+                mesh.vertex_attributes + deltas_clipped * is_inlier[..., None]
+            )
+
+            trace, _ = importance_jit(
+                jax.random.PRNGKey(2),
+                trace.get_choices(),
+                (
+                    {
+                        "num_objects": Pytree.const(1),
+                        "meshes": [mesh],
+                        "likelihood_args": likelihood_args,
+                    },
+                ),
+            )
+            return trace.get_score()
+
+        def _gvmf_and_select_best_move(
+            trace, key, variance, concentration, address, number
+        ):
+            test_poses = Pose.concatenate_poses(
+                [
+                    jax.vmap(
+                        Pose.sample_gaussian_vmf_pose, in_axes=(0, None, None, None)
+                    )(
+                        jax.random.split(key, number),
+                        trace.get_choices()[address.const],
+                        variance,
+                        concentration,
+                    ),
+                    trace.get_choices()[address.const][None, ...],
+                ]
+            )
+            scores = jax.vmap(update_pose_and_color, in_axes=(None, None, 0))(
+                trace, address, test_poses
+            )
+            trace = b3d.update_choices(
+                trace,
+                address,
+                test_poses[scores.argmax()],
+            )
+            key = jax.random.split(key, 2)[-1]
+            return trace, key
+
+        gvmf_and_select_best_move = jax.jit(
+            _gvmf_and_select_best_move, static_argnames=["number"]
+        )
+
         from b3d.chisight.dense.likelihoods.simplified_rendering_laplace_likelihood import (
             simplified_rendering_laplace_likelihood,
         )
@@ -90,15 +159,14 @@ def run_tracking(scene=None, object=None, debug=False):
         for OBJECT_INDEX in object_indices:
             print(f"Object {OBJECT_INDEX} out of {len(initial_object_poses) - 1}")
             T = 0
+
             template_pose = (
                 all_data[T]["camera_pose"].inv()
                 @ all_data[T]["object_poses"][OBJECT_INDEX]
             )
-
             rendered_rgbd = renderer.render_rgbd_from_mesh(
                 meshes[OBJECT_INDEX].transform(template_pose)
             )
-            b3d.rr_log_rgb(rendered_rgbd, "image/rgb/rendered_full_mesh")
             xyz_rendered = b3d.xyz_from_depth(rendered_rgbd[..., 3], fx, fy, cx, cy)
 
             fx, fy, cx, cy = all_data[T]["camera_intrinsics"]
@@ -116,11 +184,17 @@ def run_tracking(scene=None, object=None, debug=False):
                 vertex_attributes=all_data[T]["rgbd"][..., :3][mask],
             )
 
+            outlier_probability_sweep = jnp.array([0.05, 1.0])
+
             choicemap = genjax.ChoiceMap.d(
                 {
                     "rgbd": all_data[T]["rgbd"],
                     "camera_pose": Pose.identity(),
                     "object_pose_0": template_pose,
+                    "outlier_probability": jnp.ones(len(mesh.vertices))
+                    * outlier_probability_sweep[0],
+                    "color_noise_variance": 0.05,
+                    "depth_noise_variance": 0.01,
                 }
             )
 
@@ -145,49 +219,81 @@ def run_tracking(scene=None, object=None, debug=False):
                     Pytree.const(("rgbd",)),
                     all_data[T]["rgbd"],
                 )
-                trace, key = (
-                    b3d.bayes3d.enumerative_proposals.gvmf_and_select_best_move(
-                        trace, key, 0.04, 700.0, "object_pose_0", 20000
-                    )
-                )
-                trace, key = (
-                    b3d.bayes3d.enumerative_proposals.gvmf_and_select_best_move(
-                        trace, key, 0.03, 700.0, "object_pose_0", 20000
-                    )
-                )
-                trace, key = (
-                    b3d.bayes3d.enumerative_proposals.gvmf_and_select_best_move(
-                        trace, key, 0.02, 1000.0, "object_pose_0", 20000
-                    )
-                )
-                trace, key = (
-                    b3d.bayes3d.enumerative_proposals.gvmf_and_select_best_move(
-                        trace, key, 0.01, 1000.0, "object_pose_0", 20000
-                    )
-                )
-                tracking_results[T] = trace
 
-                if T > 0 and T % 10 == 0:
-                    template_pose = trace.get_choices()["object_pose_0"]
-                    rendered_rgbd = renderer.render_rgbd_from_mesh(
-                        meshes[OBJECT_INDEX].transform(template_pose)
+                for _ in range(5):
+                    trace, key = gvmf_and_select_best_move(
+                        trace,
+                        key,
+                        0.01,
+                        1000.0,
+                        Pytree.const(("object_pose_0",)),
+                        10000,
                     )
-                    xyz_rendered = b3d.xyz_from_depth(
-                        rendered_rgbd[..., 3], fx, fy, cx, cy
+                    trace, key = gvmf_and_select_best_move(
+                        trace,
+                        key,
+                        0.005,
+                        2000.0,
+                        Pytree.const(("object_pose_0",)),
+                        10000,
+                    )
+                # viz_trace(trace, T)
+
+                if T % 1 == 0:
+                    trace = b3d.bayes3d.enumerative_proposals.enumerate_and_select_best(
+                        trace,
+                        Pytree.const(("color_noise_variance",)),
+                        jnp.linspace(0.05, 0.1, 10),
+                    )
+                    trace = b3d.bayes3d.enumerative_proposals.enumerate_and_select_best(
+                        trace,
+                        Pytree.const(("depth_noise_variance",)),
+                        jnp.linspace(0.005, 0.01, 10),
                     )
 
-                    fx, fy, cx, cy = all_data[T]["camera_intrinsics"]
-                    xyz_observed = b3d.xyz_from_depth(
-                        all_data[T]["rgbd"][..., 3], fx, fy, cx, cy
+                    current_outlier_probabilities = trace.get_choices()[
+                        "outlier_probability"
+                    ]
+                    scores = grid_outlier_prob(
+                        trace,
+                        outlier_probability_sweep[..., None]
+                        * jnp.ones_like(current_outlier_probabilities),
                     )
-                    mask = (xyz_observed[..., 2] > 0) * (
-                        jnp.linalg.norm(xyz_rendered - xyz_observed, axis=-1) < 0.01
+                    trace = b3d.update_choices(
+                        trace,
+                        Pytree.const(("outlier_probability",)),
+                        outlier_probability_sweep[jnp.argmax(scores, axis=0)],
                     )
-                    mesh = Mesh(
-                        vertices=template_pose.inv().apply(xyz_rendered[mask]),
-                        faces=jnp.zeros((0, 3), dtype=jnp.int32),
-                        vertex_attributes=all_data[T]["rgbd"][..., :3][mask],
+
+                    current_outlier_probabilities = trace.get_choices()[
+                        "outlier_probability"
+                    ]
+                    # b3d.rr_log_cloud(
+                    #     mesh.vertices,
+                    #     colors=colors[1 * (current_outlier_probabilities == outlier_probability_sweep[0])],
+                    #     channel="cloud/outlier_probabilities"
+                    # )
+
+                    info = info_from_trace(trace)
+                    current_outlier_probabilities = trace.get_choices()[
+                        "outlier_probability"
+                    ]
+                    model_rgbd, observed_rgbd = (
+                        info["model_rgbd"],
+                        info["corresponding_observed_rgbd"],
                     )
+                    deltas = observed_rgbd - model_rgbd
+                    deltas_clipped = jnp.clip(deltas, -0.05, 0.05)
+                    new_model_rgbd = model_rgbd + deltas_clipped
+
+                    mesh = trace.get_args()[0]["meshes"][0]
+                    is_inlier = (
+                        current_outlier_probabilities == outlier_probability_sweep[0]
+                    )
+                    mesh.vertex_attributes = mesh.vertex_attributes.at[is_inlier].set(
+                        new_model_rgbd[is_inlier, :3]
+                    )
+
                     trace, _ = importance_jit(
                         jax.random.PRNGKey(2),
                         trace.get_choices(),
@@ -199,7 +305,7 @@ def run_tracking(scene=None, object=None, debug=False):
                             },
                         ),
                     )
-
+                    tracking_results[T] = trace
                 if debug:
                     viz_trace(trace, T)
 

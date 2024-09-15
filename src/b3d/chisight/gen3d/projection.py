c2eb0bfed6ce54d4d0d463ca7c9588c0429f5832
Nishad Gothoskar <nishadg@mit.edu>
2024-09-15 06:46:54 +0000

    spam working

--- src/b3d/chisight/gen3d/projection.py
+++ src/b3d/chisight/gen3d/projection.py
@@ -50,7 +50,7 @@ class PixelsPointsAssociation(Pytree):
                 intrinsics["cx"],
                 intrinsics["cy"],
             )
-            - 0.5
+            # - 0.5 ? (not sure if this is necessary) ??
         )
 
         image_height, image_width = (

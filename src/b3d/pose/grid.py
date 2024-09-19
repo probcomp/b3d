5b270ffc8adae9bd797ee181f45af13711a11f72
Karen Chung <karenchung117@gmail.com>
2024-09-19 19:35:11 +0000

    switch to half dimensions for everything; tested

--- src/b3d/pose/grid.py
+++ src/b3d/pose/grid.py
@@ -13,7 +13,7 @@ from b3d import Mesh
 from .core import Pose
 
 
-def viz_rotation(pose, vertices, t=0, channel="scene/transformed_mesh"):
+def viz_rotation(pose, vertices, t=0, channel="mesh/xfm_cloud"):
     """visualize a rotation of a given mesh vertex set in rerun"""
     b3d.rr_set_time(t)
 
@@ -32,7 +32,7 @@ def viz_rotation(pose, vertices, t=0, channel="scene/transformed_mesh"):
             media_type=rr.MediaType.MARKDOWN,
         ),
     )
-    b3d.rr_log_pose(pose, "pose/xfm_pose")
+    b3d.rr_log_pose(pose, "axes/xfm_pose_axes")
 
 
 def viz_from_grid(pose_grid, rerun_session_name="grid_test", ycb_obj_id=13):
@@ -48,12 +48,25 @@ def viz_from_grid(pose_grid, rerun_session_name="grid_test", ycb_obj_id=13):
 
     # setup viz
     b3d.rr_init(rerun_session_name)
-    b3d.rr_log_pose(Pose.identity(), "pose/default_pose")
-    viz_rotation(Pose.identity(), mesh_vertices, 0, "pose/default_pose")
+
+    # log the default pose
+    b3d.rr_set_time(0)
+    b3d.rr_log_pose(Pose.identity(), "axes/default_pose_axes")
+    viz_rotation(Pose.identity(), mesh_vertices, 0, "mesh/default_pose_cloud")
 
     # visualize
     for t, pose_viz in enumerate(pose_grid):
-        viz_rotation(pose_viz, mesh_vertices, t + 1)
+        viz_rotation(pose_viz, mesh_vertices, t + 1, "mesh/xfm_cloud")
+
+
+def sorted_linspace(delta, half_num):
+    if half_num == 0:  # only the zero-transform sample is returned
+        return jnp.array([0.0])
+
+    num_samples = half_num * 2 + 1
+    linspace = jnp.linspace(-delta, delta, num_samples)
+    ordered_linspace = linspace[jnp.argsort(jnp.abs(linspace))]
+    return ordered_linspace
 
 
 def rr_log_pose_arrows_grid(pose_grid, channel="pose_grid", scale=0.02):
@@ -65,19 +78,18 @@ def rr_log_pose_arrows_grid(pose_grid, channel="pose_grid", scale=0.02):
 
 
 def make_rotation_grid_enumeration(
-    min_angle,
-    max_angle,
-    n_alpha,
-    n_beta,
-    n_gamma,
+    half_d_angle,
+    half_n_alpha,
+    half_n_beta,
+    half_n_gamma,
 ) -> Pose:
     """
     Enumerate rotations via euler angles uniformly gridded in the range [min_angle, max_angle]
     for each axis (angle of axes: X = alpha, Y = beta, Z = gamma)
     """
-    alphas = jnp.linspace(min_angle, max_angle, n_alpha)
-    betas = jnp.linspace(min_angle, max_angle, n_beta)
-    gammas = jnp.linspace(min_angle, max_angle, n_gamma)
+    alphas = sorted_linspace(half_d_angle, half_n_alpha)
+    betas = sorted_linspace(half_d_angle, half_n_beta)
+    gammas = sorted_linspace(half_d_angle, half_n_gamma)
 
     # nest vmap over all axes
     def _inner_proposal(alpha, beta, gamma):
@@ -89,6 +101,11 @@ def make_rotation_grid_enumeration(
     _proposal_zy = lambda gamma: jax.vmap(_proposal_z, in_axes=(None, 0))(gamma, betas)  # noqa:E731
     proposal_zyx = jax.vmap(_proposal_zy, in_axes=(0,))(gammas)
 
+    n_alpha, n_beta, n_gamma = (
+        2 * half_n_alpha + 1,
+        2 * half_n_beta + 1,
+        2 * half_n_gamma + 1,
+    )
     return Pose(
         jnp.zeros((n_alpha * n_beta * n_gamma, 3)),
         proposal_zyx.as_quat().reshape(-1, 4),
@@ -96,48 +113,49 @@ def make_rotation_grid_enumeration(
 
 
 def make_translation_grid_enumeration(
-    min_x, min_y, min_z, max_x, max_y, max_z, num_x, num_y, num_z
-):
+    half_dx, half_dy, dz, half_num_x, half_num_y, half_num_z
+) -> Pose:
     """
     Generate uniformly spaced translation proposals in a 3D box
     Args:
-        min_x, min_y, min_z: minimum x, y, z values
+        half_dx, half_dy, dz: half-dimension of each of the x, y, z directions
+        half_num_x, half_num_y, half_num_z: samples in each of the dimensions, EXCLUDING the zero sample.
     """
+    x_space = sorted_linspace(half_dx, half_num_x)
+    y_space = sorted_linspace(half_dy, half_num_y)
+    z_space = sorted_linspace(dz, half_num_z)
     deltas = jnp.stack(
         jnp.meshgrid(
-            jnp.linspace(min_x, max_x, num_x),
-            jnp.linspace(min_y, max_y, num_y),
-            jnp.linspace(min_z, max_z, num_z),
+            x_space,
+            y_space,
+            z_space,
         ),
         axis=-1,
     )
     deltas = deltas.reshape((-1, 3), order="F")
 
+    num_x, num_y, num_z = 2 * half_num_x + 1, 2 * half_num_y + 1, 2 * half_num_z + 1
     return Pose(deltas, jnp.tile(Pose.identity_quaternion, (num_x * num_y * num_z, 1)))
 
 
 def pose_grid(
     pose_center: Pose,
-    min_x: float,
-    min_y: float,
-    min_z: float,
-    max_x: float,
-    max_y: float,
-    max_z: float,
-    nx: int,
-    ny: int,
-    nz: int,
-    min_euler_angle: float,
-    max_euler_angle: float,
-    n_xrot: int,
-    n_yrot: int,
-    n_zrot: int,
-) -> jnp.ndarray:
+    half_dx=float,
+    half_dy=float,
+    half_dz=float,
+    half_nx=int,
+    half_ny=int,
+    half_nz=int,
+    half_dangle=float,
+    half_n_xrot=int,
+    half_n_yrot=int,
+    half_n_zrot=int,
+) -> Pose:
     tr_grid = make_translation_grid_enumeration(
-        min_x, min_y, min_z, max_x, max_y, max_z, nx, ny, nz
+        half_dx, half_dy, half_dz, half_nx, half_ny, half_nz
     )
     rot_grid = make_rotation_grid_enumeration(
-        min_euler_angle, max_euler_angle, n_xrot, n_yrot, n_zrot
+        half_dangle, half_n_xrot, half_n_yrot, half_n_zrot
     )
 
     compose_pose = lambda tr, rot: pose_center.compose(tr).compose(rot)  # noqa:E731
@@ -152,63 +170,61 @@ def pose_grid(
 
 
 if __name__ == "__main__":
+    #################################
+    # Setup test
+    #################################
+    VIZ_TEST = False  # toggle to visualize all grid on rerun
+
     # center pose
     xyz0 = jnp.array([0.0, 0.0, 0.0])
     rot0 = jnp.array([0.0, 0.0, 0.0, 1.0])
     pose0 = Pose(xyz0, rot0)
 
     # translation grid
-    nx = ny = nz = 5
-    ntr = nx * ny * nz
+    half_nx = half_ny = half_nz = 2
+    ntr = (2 * half_nx + 1) * (2 * half_ny + 1) * (2 * half_nz + 1)
     print(f"Generating {ntr} translations")
-    min_x, min_y, min_z = -0.1, -0.1, 0.001
-    max_x, max_y, max_z = 0.1, 0.1, 0.2
+    half_dx, half_dy, half_dz = 1, 1, 0.001
 
     # rotation grid
-    n_alpha = n_beta = n_gamma = 5
-    nrot = n_alpha * n_beta * n_gamma
+    half_n_alpha = half_n_beta = half_n_gamma = 2
+    half_d_euler = jnp.pi / 3
+    nrot = (2 * half_n_alpha + 1) * (2 * half_n_beta + 1) * (2 * half_n_gamma + 1)
     print(f"Generating {nrot} rotations")
-    min_euler, max_euler = -jnp.pi / 6, jnp.pi / 6
 
     ##################################
-    # Translation / Rotation grids
-    ##################################
-    tr_grid = make_translation_grid_enumeration(
-        min_x, min_y, min_z, max_x, max_y, max_z, nx, ny, nz
-    )
-    rot_grid = make_rotation_grid_enumeration(
-        min_euler, max_euler, n_alpha, n_beta, n_gamma
-    )
-
-    ##################################
-    # Whole pose grid
+    # Generate pose grid
     ##################################
 
     pose_grid_jit = jax.jit(
-        pose_grid, static_argnames=("nx", "ny", "nz", "n_xrot", "n_yrot", "n_zrot")
+        pose_grid,
+        static_argnames=(
+            "half_nx",
+            "half_ny",
+            "half_nz",
+            "half_n_xrot",
+            "half_n_yrot",
+            "half_n_zrot",
+        ),
     )
     grid = pose_grid_jit(
         pose0,
-        min_x=min_x,
-        min_y=min_y,
-        min_z=min_z,
-        max_x=max_x,
-        max_y=max_y,
-        max_z=max_z,
-        nx=nx,
-        ny=ny,
-        nz=ny,
-        min_euler_angle=min_euler,
-        max_euler_angle=max_euler,
-        n_xrot=n_alpha,
-        n_yrot=n_beta,
-        n_zrot=n_gamma,
+        half_dx=half_dx,
+        half_dy=half_dy,
+        half_dz=half_dz,
+        half_nx=half_nx,
+        half_ny=half_ny,
+        half_nz=half_nz,
+        half_dangle=half_d_euler,
+        half_n_xrot=half_n_alpha,
+        half_n_yrot=half_n_beta,
+        half_n_zrot=half_n_gamma,
     )
 
     ##################################
     # Test correctness
     ##################################
-    # sanity check sizes
+    # 1a. sanity check sizes
     assert isinstance(
         grid, Pose
     ), f"Wrong return type; expected b3d.Pose, got {type(grid)}"
@@ -221,30 +237,40 @@ if __name__ == "__main__":
         4,
     ), f"Wrong shape for quat; expected {(ntr * nrot, 4)}, got {grid.quaternion.shape}"
 
-    # viz_from_grid(grid, rerun_session_name=f"GRID_{ntr}_{nrot}", ycb_obj_id=13)
+    # 1b. check that original pose is in grid
+    assert (
+        grid.pos.tolist().index(pose0.pos.tolist())
+        == grid.quat.tolist().index(pose0.quat.tolist())
+        != -1
+    ), "Center pose not in grid"
+    print("Size checks and center-pose checks passed")
+
+    # 2. visualize grid
+    if VIZ_TEST:
+        print(f"Visualizing {ntr*nrot} poses in rerun...")
+        viz_from_grid(grid, rerun_session_name=f"GRID_{ntr}_{nrot}", ycb_obj_id=13)
+    else:
+        print("Skipping visualization...")
 
     # ##################################
     # # Test time
     # ##################################
     import time
 
+    print("Testing jitted runtime...")
     start = time.time()
     timed_grid = pose_grid_jit(
         pose0,
-        min_x=min_x,
-        min_y=min_y,
-        min_z=min_z,
-        max_x=max_x,
-        max_y=max_y,
-        max_z=max_z,
-        nx=nx,
-        ny=ny,
-        nz=ny,
-        min_euler_angle=min_euler,
-        max_euler_angle=max_euler,
-        n_xrot=n_alpha,
-        n_yrot=n_beta,
-        n_zrot=n_gamma,
+        half_dx=half_dx,
+        half_dy=half_dy,
+        half_dz=half_dz,
+        half_nx=half_nx,
+        half_ny=half_ny,
+        half_nz=half_nz,
+        half_dangle=half_d_euler,
+        half_n_xrot=half_n_alpha,
+        half_n_yrot=half_n_beta,
+        half_n_zrot=half_n_gamma,
     )
     end = time.time()
     print(f"Time taken: {(end-start)*1000} milliseconds for {ntr*nrot} poses")

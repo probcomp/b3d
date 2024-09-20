2bbe13cc52df8c0f2c85e39646b8de6ba621e8a1
nishadgothoskar <nishadg@mit.edu>
2024-09-19 21:58:29 -0400

    Fix downloader and tests (#198)

--- scripts/download_ycbv_train.sh
+++ scripts/download_ycbv_train.sh
@@ -1,4 +1,5 @@
 export SRC=https://huggingface.co/datasets/bop-benchmark/datasets/resolve/main
 wget $SRC/ycbv/ycbv_train_real.zip -P assets/bop
+wget $SRC/ycbv/ycbv_train_real.z01 -P assets/bop
 zip -s0 assets/bop/ycbv_train_real.zip --out assets/bop/ycbv_train_real_all.zip
 unzip assets/bop/ycbv_train_real_all.zip -d assets/bop/ycbv

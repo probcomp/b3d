f01701a6e7ed50275a422c7889f921ccebe446ca
George Matheos <georgematheos@gmail.com>
2024-09-20 16:52:26 +0000

    first debugging log

---
+++ src/b3d/chisight/gen3d/__init__.py
@@ -0,0 +1,7 @@
+from . import dataloading as dataloading
+from . import hyperparams as hyperparams
+from . import metrics as metrics
+from . import model as model
+from . import settings as settings
+from .inference import inference as inference
+from .inference import point_attribute_proposals as point_attribute_proposals

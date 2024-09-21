d87dde2392ebd71ef685988d692c45be6149106a
George Matheos <georgematheos@gmail.com>
2024-09-21 01:07:45 +0000

    cleanup

--- src/b3d/chisight/gen3d/settings.py
+++ src/b3d/chisight/gen3d/settings.py
@@ -18,20 +18,27 @@ from b3d.chisight.gen3d.pixel_kernels.pixel_rgbd_kernels import (
 p_resample_color = 0.005
 hyperparams = {
     "pose_kernel": transition_kernels.GaussianVMFPoseDriftKernel(0.02, 1000.0),
-    "color_kernel": transition_kernels.RenormalizedLaplaceColorDriftKernel(scale=0.05),
+    "color_kernel": transition_kernels.MixtureDriftKernel(
+        [
+            transition_kernels.RenormalizedLaplaceColorDriftKernel(scale=0.05),
+            transition_kernels.UniformDriftKernel(
+                max_shift=0.15, min_val=jnp.zeros(3), max_val=jnp.ones(3)
+            ),
+        ],
+        jnp.array([1 - p_resample_color, p_resample_color]),
+    ),
     "visibility_prob_kernel": transition_kernels.DiscreteFlipKernel(
-        p_change_to_different_value=0.05, support=jnp.array([0.0, 1.0])
+        p_change_to_different_value=0.1, support=jnp.array([0.001, 0.999])
     ),
     "depth_nonreturn_prob_kernel": transition_kernels.DiscreteFlipKernel(
-        p_change_to_different_value=0.1, support=jnp.array([0.0, 1.0])
+        p_change_to_different_value=0.1, support=jnp.array([0.001, 0.999])
     ),
     "depth_scale_kernel": transition_kernels.DiscreteFlipKernel(
         p_change_to_different_value=0.1,
-        support=jnp.array([0.001, 0.0025, 0.01, 0.02]),
+        support=jnp.array([0.0025, 0.01, 0.02]),
     ),
     "color_scale_kernel": transition_kernels.DiscreteFlipKernel(
-        p_change_to_different_value=0.1,
-        support=jnp.array([0.05, 0.1, 0.15]),
+        p_change_to_different_value=0.1, support=jnp.array([0.05, 0.1, 0.15])
     ),
     "image_kernel": image_kernel.UniquePixelsImageKernel(
         FullPixelRGBDDistribution(
